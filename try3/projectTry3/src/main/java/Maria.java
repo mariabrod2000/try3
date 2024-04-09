98ec5b6ca07f4f1869b31cd2bfec5be950208c03
mariabrod2000 <mariabrodsky@yahoo.com>
2024-04-09 15:24:05 -0400

    so interesting

---
+++ try3/projectTry3/src/main/java/Maria.java
@@ -0,0 +1,8 @@
+public class Maria {
+
+        public static void main(String[] args) {
+
+        System.out.println("Java drives the Web1");
+            System.out.println("Java drives the Web1");
+    }
+}
